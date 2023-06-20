276d0ca7cc368d52f16a3a348a6b4189a6a87d40
geeky01adarsh <adarsh91094@gmail.com>
2023-06-20 16:45:20 +0530

    login in react

---
+++ auth-react/src/LoginPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Perform login logic here, such as making API calls
+    console.log("Login form submitted");
+    console.log("Email:", email);
+    console.log("Password:", password);
+    const response = await axios.post("http://localhost:5000/api/v1/auth/login/", {
+      email, password
+    });
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
